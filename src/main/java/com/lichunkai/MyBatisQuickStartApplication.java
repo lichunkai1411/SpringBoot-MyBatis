3dfbd58b4ef6021e5a350df0408c965b1bfbf07b
刘和 <test@123.com>
2023-04-11 11:18:55 +0800

    MyBatis快速入门

---
+++ src/main/java/com/lichunkai/MyBatisQuickStartApplication.java
@@ -0,0 +1,13 @@
+package com.lichunkai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisQuickStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisQuickStartApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lichunkai/mapper/UserMapper.java
@@ -0,0 +1,4 @@
+package com.lichunkai.mapper;
+
+public interface UserMapper {
+}
