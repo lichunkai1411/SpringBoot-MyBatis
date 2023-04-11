06fcc5c8739a215e19964c5d7f93a2e96648f5e5
李春凯 <test@123.com>
2023-04-11 12:00:19 +0800

    完成快速入门

--- src/main/java/com/lichunkai/pojo/User.java
+++ src/main/java/com/lichunkai/pojo/User.java
@@ -1,110 +1,18 @@
 package com.lichunkai.pojo;
 
+import lombok.Data;
+
 /**
  * Created by IntelliJ IDEA.
  * Author：Lick
  * Date Created in 2023-04-11
  * Time：11:07
  */
+@Data
 public class User {
     private Integer id;
     private String name;
     private short age;
     private short gender;
     private String phone;
-
-   public User() {
-   }
-
-   public User(Integer id, String name, short age, short gender, String phone) {
-      this.id = id;
-      this.name = name;
-      this.age = age;
-      this.gender = gender;
-      this.phone = phone;
-   }
-
-   /**
-    * 获取
-    * @return id
-    */
-   public Integer getId() {
-      return id;
-   }
-
-   /**
-    * 设置
-    * @param id
-    */
-   public void setId(Integer id) {
-      this.id = id;
-   }
-
-   /**
-    * 获取
-    * @return name
-    */
-   public String getName() {
-      return name;
-   }
-
-   /**
-    * 设置
-    * @param name
-    */
-   public void setName(String name) {
-      this.name = name;
-   }
-
-   /**
-    * 获取
-    * @return age
-    */
-   public short getAge() {
-      return age;
-   }
-
-   /**
-    * 设置
-    * @param age
-    */
-   public void setAge(short age) {
-      this.age = age;
-   }
-
-   /**
-    * 获取
-    * @return gender
-    */
-   public short getGender() {
-      return gender;
-   }
-
-   /**
-    * 设置
-    * @param gender
-    */
-   public void setGender(short gender) {
-      this.gender = gender;
-   }
-
-   /**
-    * 获取
-    * @return phone
-    */
-   public String getPhone() {
-      return phone;
-   }
-
-   /**
-    * 设置
-    * @param phone
-    */
-   public void setPhone(String phone) {
-      this.phone = phone;
-   }
-
-   public String toString() {
-      return "User{id = " + id + ", name = " + name + ", age = " + age + ", gender = " + gender + ", phone = " + phone + "}";
-   }
 }
--- src/test/java/com/lichunkai/MyBatisQuickStartApplicationTests.java
+++ src/test/java/com/lichunkai/MyBatisQuickStartApplicationTests.java
@@ -1,13 +1,23 @@
 package com.lichunkai;
 
+import com.lichunkai.mapper.UserMapper;
+import com.lichunkai.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MyBatisQuickStartApplicationTests {
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
-    void contextLoads() {
+    public void testListUser() {
+        List<User> userList = userMapper.list();
+        for (User user : userList) {
+            System.out.println(user);
+        }
     }
-
 }
