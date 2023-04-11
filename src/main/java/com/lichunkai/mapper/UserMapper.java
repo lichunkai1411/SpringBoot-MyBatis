04790101208f435d3a8887ffb6dd924fe5c972cf
李春凯 <test@123.com>
2023-04-11 11:33:59 +0800

    完成Mapper创建

--- src/main/java/com/lichunkai/mapper/UserMapper.java
+++ src/main/java/com/lichunkai/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package com.lichunkai.mapper;
 
+import com.lichunkai.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper // 在运行时,会自动生成该接口的实现类对对象(代理对象),并且将该对象交给IOC容器管理
 public interface UserMapper {
+    // 查询全部用户信息
+    @Select("select * from user")
+    List<User> list();
 }
